63d12918ebd303d9be906b6358b80c7b47753ee5
David Kristinsson <david@aurbjorg.is>
2025-07-23 22:18:03 +0000

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // Pages
 import HomePage from './pages/HomePage';
 import AdminDashboard from './pages/AdminDashboard';
-import SupabaseTest from './pages/SupabaseTest';
 import RegistrationDebug from './pages/RegistrationDebug';
 
 function App() {
@@ -13,7 +11,6 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/admin" element={<AdminDashboard />} />
-        <Route path="/test" element={<SupabaseTest />} />
         <Route path="/debug" element={<RegistrationDebug />} />
       </Routes>
     </Router>
